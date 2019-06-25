4aab232a022f0959e357eac057387c57fad0886c
DenisCalixto <deniscalixto@gmail.com>
2019-06-25 00:14:38 -0700

    API route for bringing 5 leaders of Zero wasting

---
+++ web-server/classes/company.js
@@ -0,0 +1,34 @@
+const Company = class Company {
+
+    constructor(name, website, city, province, address, expiringItemnsNote, provideReusableItemsNotes, 
+                recentHistory, comments, loosePercentage, discountExpiringItems, donateExpiringItems, 
+                throwOutExpiringItems, sellInBulk, byo, extraChargeSingleItem, provideReusableItems, 
+                employeesUseSingleUseItems, employeesSingleUseItemsNotes) {
+
+        this.name = name;
+        this.website = website;
+        this.city = city;
+        this.province = province;
+        this.address = address;
+        this.expiringItemnsNote = expiringItemnsNote;
+        this.provideReusableItemsNotes = provideReusableItemsNotes;
+        this.recentHistory = recentHistory;
+        this.comments = comments;
+        this.loosePercentage = loosePercentage;
+        this.discountExpiringItems = discountExpiringItems;
+        this.donateExpiringItems = donateExpiringItems;
+        this.throwOutExpiringItems = throwOutExpiringItems;
+        this.sellInBulk = sellInBulk;
+        this.byo = byo;
+        this.extraChargeSingleItem = extraChargeSingleItem;
+        this.provideReusableItems = provideReusableItems;
+        this.employeesUseSingleUseItems = employeesUseSingleUseItems;
+        this.employeesSingleUseItemsNotes = employeesSingleUseItemsNotes;
+        
+    }
+
+}
+
+module.exports = {
+    Company
+}
---
+++ web-server/lib/boards.js
@@ -0,0 +1,40 @@
+let mysql = require('mysql');
+
+let connection = require('../lib/db');
+
+const saveCompany = (company) => {
+    // query = 'INSERT INTO `refillitem` (RefillId, ProductCategoryId, Quantity)' +
+    //         'Values (' + refillId + ',' + productCategoryId + ',' + quantity + ')';
+    // connection.query(query, function(err, result) {
+    //     if (err) {
+    //         return err
+    //     }
+    // })
+}
+
+const saveCompanyBulkStrategies = (refillId, productCategoryId, quantity) => {
+    query = 'INSERT INTO `refillitem` (RefillId, ProductCategoryId, Quantity)' +
+            'Values (' + refillId + ',' + productCategoryId + ',' + quantity + ')';
+    connection.query(query, function(err, result) {
+        if (err) {
+            return err
+        }
+    })
+}
+
+const saveCompanyProductCategories = (refillId, productCategoryId, quantity) => {
+    query = 'INSERT INTO `refillitem` (RefillId, ProductCategoryId, Quantity)' +
+            'Values (' + refillId + ',' + productCategoryId + ',' + quantity + ')';
+    connection.query(query, function(err, result) {
+        if (err) {
+            return err
+        }
+    })
+}
+
+
+module.exports = {
+    saveCompany, 
+    saveCompanyBulkStrategies, 
+    saveCompanyProductCategories
+}
